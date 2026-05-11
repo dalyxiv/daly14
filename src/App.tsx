b5770b8a1bb88156dc3af4d486c36261eee384ca
Mostafa Eldaly <dalyxiv@gmail.com>
2026-05-12 01:40:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* The line below is the one that changed! */}
+      <BrowserRouter basename="/daly14">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
